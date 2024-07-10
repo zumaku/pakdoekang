e22feccbaf0993c30671fd3eda2e8618dccaaab8
zumaku <60200121101@uin-alauddin.ac.id>
2024-07-10 08:38:41 +0800

    feat(ui): Membuat cartu pengeluaran harian

--- lib/pages/today_page.dart
+++ lib/pages/today_page.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:pakdoekang/widgets/cards/daily_spend_card.dart';
 
 class TodayPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text('Halaman Hari Ini'),
-    );
+        child: SafeArea(
+      minimum: EdgeInsets.all(20),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          DailySpendCard(),
+        ],
+      ),
+    ));
   }
 }
---
+++ lib/widgets/cards/daily_spend_card.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:pakdoekang/widgets/my_colors.dart';
+import 'package:pakdoekang/widgets/my_shadow.dart';
+import 'package:pakdoekang/widgets/my_text.dart';
+
+class DailySpendCard extends StatelessWidget {
+  const DailySpendCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      // padding: EdgeInsets.symmetric(vertical: 10, horizontal: 16),
+      decoration: BoxDecoration(
+        color: MyColor.brand2,
+        borderRadius: BorderRadius.circular(10),
+        boxShadow: [MyShadow.shadowOne()],
+      ),
+      child: Stack(
+        children: <Widget>[
+          // Positioned widget for overlay
+          Positioned(
+            top: -40,
+            right: 0,
+            child: MyText.headingOne("12", color: MyColor.brand1),
+          ),
+          Positioned(
+            top: 40,
+            right: 0,
+            child: MyText.headingOne("Jul", color: MyColor.brand1),
+          ),
+
+          // Background widget
+          Container(
+            padding: EdgeInsets.symmetric(vertical: 10, horizontal: 16),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    MyText.headingFour("Total Pengeluaran",
+                        color: MyColor.brand4),
+                    MyText.headingFive("Rp. 42.000")
+                  ],
+                ),
+                SizedBox(height: 20),
+                MyText.subTitleTwo("Pemasukan: Rp. 50.000")
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
