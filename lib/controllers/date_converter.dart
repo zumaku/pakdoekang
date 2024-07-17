5e4f2f62b0abe08e8d9198ced1501fe9a3f2d639
zumaku <60200121101@uin-alauddin.ac.id>
2024-07-17 10:29:06 +0800

    style: fix date selector.
    Add more conditional statement

--- lib/controllers/date_converter.dart
+++ lib/controllers/date_converter.dart
@@ -2,11 +2,13 @@ import 'package:intl/intl.dart';
 
 class DateConverter {
   static String convert(DateTime date) {
-    if (DateTime.now().day == date.day) {
+    if (DateTime.now() == date) {
       return "Hari ini - ${date.hour}:${date.minute}";
-    } else if (DateTime.now().day - 1 == date.day) {
+    } else if (DateTime.now().day - 1 == date.day &&
+        DateTime.now().month == date.month) {
       return "Kemarin - ${date.hour}:${date.minute}";
-    } else if (DateTime.now().day + 1 == date.day) {
+    } else if (DateTime.now().day + 1 == date.day &&
+        DateTime.now().month == date.month) {
       return "Besok - ${date.hour}:${date.minute}";
     } else {
       return "${DateFormat('d MMM').format(date)} - ${date.hour}:${date.minute}";
