4385b1f6a4ec8483ea88f5587b39a9340320a979
zumaku <60200121101@uin-alauddin.ac.id>
2024-07-17 12:30:22 +0800

    fix: Remove center widget

--- lib/pages/today_page.dart
+++ lib/pages/today_page.dart
@@ -13,71 +13,68 @@ import 'package:skeleton_loader/skeleton_loader.dart';
 class TodayPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-        child: SingleChildScrollView(
-            child: SafeArea(
-                minimum: EdgeInsets.all(20),
-                child: Column(
-                    // mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    children: [
-                      DailySpendCard(),
-                      SizedBox(height: 26),
-                      MyText.headingSix("Aktifitas",
-                          textAlign: TextAlign.start),
-                      SizedBox(height: 13),
-                      StreamBuilder<List<Transaksi>>(
-                          stream: Provider.of<FirestoreServiceProvider>(context)
-                              .getTransaksi(DateTime.now()),
-                          builder: (context, snapshot) {
-                            if (snapshot.connectionState ==
-                                ConnectionState.waiting) {
-                              return SkeletonLoader(
-                                // Customize your skeleton loader here
-                                builder: Container(
-                                  height: 100,
-                                  margin: EdgeInsets.symmetric(vertical: 10.0),
-                                  decoration: BoxDecoration(
-                                    color: MyColor.base5,
-                                    borderRadius: BorderRadius.circular(16),
-                                  ),
-                                ),
-                                items: 3,
-                                period: Duration(seconds: 1),
-                              );
-                            }
-                            if (snapshot.hasError) {
-                              return Center(
-                                  child: MyText.paragraphOne(
-                                      'Error: ${snapshot.error}'));
-                            }
-                            if (!snapshot.hasData || snapshot.data!.isEmpty) {
-                              return Container(
-                                height:
-                                    MediaQuery.of(context).size.height / 2 - 20,
-                                child: Center(
-                                  child: MyText.labelOne("Belum ada transaksi",
-                                      color: MyColor.base),
-                                ),
-                              );
-                            }
+    return SingleChildScrollView(
+        child: SafeArea(
+            minimum: EdgeInsets.all(20),
+            child: Column(
+                // mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  DailySpendCard(),
+                  SizedBox(height: 26),
+                  MyText.headingSix("Aktifitas", textAlign: TextAlign.start),
+                  SizedBox(height: 13),
+                  StreamBuilder<List<Transaksi>>(
+                      stream: Provider.of<FirestoreServiceProvider>(context)
+                          .getTransaksi(DateTime.now()),
+                      builder: (context, snapshot) {
+                        if (snapshot.connectionState ==
+                            ConnectionState.waiting) {
+                          return SkeletonLoader(
+                            // Customize your skeleton loader here
+                            builder: Container(
+                              height: 100,
+                              margin: EdgeInsets.symmetric(vertical: 10.0),
+                              decoration: BoxDecoration(
+                                color: MyColor.base5,
+                                borderRadius: BorderRadius.circular(16),
+                              ),
+                            ),
+                            items: 3,
+                            period: Duration(seconds: 1),
+                          );
+                        }
+                        if (snapshot.hasError) {
+                          return Center(
+                              child: MyText.paragraphOne(
+                                  'Error: ${snapshot.error}'));
+                        }
+                        if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                          return Container(
+                            height: MediaQuery.of(context).size.height / 2 - 20,
+                            child: Center(
+                              child: MyText.labelOne("Belum ada transaksi",
+                                  color: MyColor.base),
+                            ),
+                          );
+                        }
 
-                            final transactions = snapshot.data!;
-                            return Column(
-                              children: transactions.map((transaction) {
-                                return ActivityCard(
-                                  id_activity: transaction.id,
-                                  activity: transaction.aktifitas,
-                                  amount: transaction.jumlah,
-                                  date: transaction.tanggal,
-                                  categories: transaction.kategori,
-                                  isSpend: transaction.isPengeluaran,
-                                  notes: transaction.catatan,
-                                );
-                              }).toList(),
+                        final transactions = snapshot.data!;
+                        return Column(
+                          children: transactions.map((transaction) {
+                            return ActivityCard(
+                              id_activity: transaction.id,
+                              activity: transaction.aktifitas,
+                              amount: transaction.jumlah,
+                              date: transaction.tanggal,
+                              categories: transaction.kategori,
+                              isSpend: transaction.isPengeluaran,
+                              notes: transaction.catatan,
                             );
-                          }),
-                    ]))));
+                          }).toList(),
+                        );
+                      }),
+                ])));
   }
 
   static Widget addActivity() {
